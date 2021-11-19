5a8c8482e2d90c85c8c65b2d00e55248cb26e1cc
franksosa14 <gonza123e@gmail.com>
2021-11-18 21:01:04 -0300

    copia de seguridad

---
+++ frontend/src/componentes/cartel.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Cartel = () => {
+    return (
+        <div className="cartel-cuerpo">
+                    <h2 className="subtitulo">My Tinerary</h2>
+                    <div>
+                        <p className="p-cartel">Find your perfect trip, designed<br></br> by insiders
+                        who know and love their cities!</p>
+                    </div>
+                    <div className="boton-cont">                      
+                       <a className="boton">Choose destination now!</a>
+                    </div>
+                </div>
+    )
+}
+
+export default Cartel
