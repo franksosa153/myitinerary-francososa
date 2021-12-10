f8a6051e9476bfc6f6dddac21117a6864c760d50
franksosa14 <gonza123e@gmail.com>
2021-12-09 22:42:09 -0300

    prueba

--- routes/routes.js
+++ routes/routes.js
@@ -3,14 +3,11 @@ const Router = require("express").Router();
 const citiesControllers=require("../controllers/citiesControllers")
 const itinerarysControllers=require("../controllers/itinerarysController")
 const validator = require('../config/validator')
-const authControllers = require('../controllers/personasControllers')
+const authControllers = require('../controllers/personasControllers');
+const { Route } = require("express");
 const {obtenerTodasLasCiudades, cargarUnaCiudad, obtenerUnaCiudad} = citiesControllers
 const {obtenerTodasLosItinerarios,  cargarUnItinerario, obtenerUnItinerario,borrarUnItinerario,modificarUnItinerario,obtenerItinerariosCiudades} = itinerarysControllers
-<<<<<<< HEAD
 const { newUser, accederACuenta } = authControllers
-=======
-const { nuevoUsuario, accederACuenta } = authControllers
->>>>>>> b502c20ece3069757ac7c4cdb2b1d8f4e782e828
 Router.route('/cities')
 .get(obtenerTodasLasCiudades)
 .post(cargarUnaCiudad) 
@@ -29,11 +26,7 @@ Router.route('/itinerary/:id')
 .get(obtenerUnItinerario)
 
 Router.route('/auth/signUp')
-<<<<<<< HEAD
 .post(validator, newUser)
-=======
-.post(validator, nuevoUsuario)
->>>>>>> b502c20ece3069757ac7c4cdb2b1d8f4e782e828
 
 Router.route('/auth/signIn')
 .post(accederACuenta)
